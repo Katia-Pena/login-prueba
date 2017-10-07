f7cece61bccf3e7638735edb2993e7356198efab
Chido <ady.jack@hotmail.com>
2017-10-07 12:34:19 -0500

    Validación de login

--- app/src/main/java/com/katy/practica1/LoginActivity.java
+++ app/src/main/java/com/katy/practica1/LoginActivity.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.support.annotation.StyleRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.BindViews;
+import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    @BindView(R.id.edt_email)
+    EditText email;
+
+    @BindView(R.id.edt_password)
+    EditText password;
+
+    @BindView(R.id.text_email)
+    TextInputLayout textEmail;
+    @BindView(R.id.text_password)
+    TextInputLayout textPassword;
+
     private static final String TAG = LoginActivity.class.getName();
 
     @Override
@@ -28,6 +45,45 @@ public class LoginActivity extends BaseActivity {
     private void initCofigToolbar(){
         toolbar=getToolbar();
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp));
+
+    }
+
+    @OnClick(R.id.btn_entrar)
+        public void onClickLogin(){
+            if(isValidLogin()){
+
+            }
+
+    }
+    private boolean isValidLogin(){
+        boolean valid=true;
+        textEmail.setError(null);
+        textEmail.setErrorEnabled(false);
+        textPassword.setError(null);
+        textPassword.setErrorEnabled(false);
+            if(email.getText().toString().isEmpty()){
+                valid=false;
+                textEmail.setError(getString(R.string.login_text_error_is_empty_email));
+            }if(password.getText().toString().isEmpty()){
+                valid=false;
+                textPassword.setError(getString(R.string.login_text_error_is_empty_password));
+            }
+            if (!isEmailValid(email.getText().toString())){
+                valid = false;
+                textEmail.setError(getString(R.string.login_text_error_is_valid_email));
+
+            }
+        return valid;
+
+    }
+
+    private boolean isEmailValid(CharSequence charSequence){
+        boolean valid=true;
+            if(TextUtils.isEmpty(charSequence)){
+                return false;
+            }else{
+                return Patterns.EMAIL_ADDRESS.matcher(charSequence).matches();
+            }
 
     }
     /**
